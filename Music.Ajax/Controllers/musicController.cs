da2cde85d2109179083dda99b95267868a588854
吕静 <@LLL-JJ>
2020-04-13 14:26:10 +0800

    添加项目文件。

---
+++ Music.Ajax/Controllers/musicController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Music.Ajax.Controllers
+{
+    public class musicController : Controller
+    {
+        // GET: music
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Show()
+        {
+            return View();
+        }
+    }
+}
---
+++ Music.Ajax/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Music.Ajax.MvcApplication" Language="C#" %>
